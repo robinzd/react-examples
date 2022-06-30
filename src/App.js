bc5f41ef68457a0e4a9925e25d06d8ef2d880018
robin <ibots.robin@gmail.com>
2022-06-30 17:07:01 +0530

    react js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,82 @@
+// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// basic syntax of the jsx file//
+// const user = {
+//   fname: 'Robinson',
+//   lastname: 'Rajiv'
+// }
+
+// function functionname(user) {
+//   return user.fname + ' ' + user.lastname;
+// }
+
+// const name = 'Divya';
+// const hello = <h1>{functionname(user)} Weds {name} !</h1>;
+// const hello1 = <h1>Hello,{name}</h1>
+
+// function App() {
+//   return (
+//     <div>
+//       {hello}
+//       {hello1}
+//     </div>
+
+//   );
+// }
+// basic syntax of the jsx file//
+
+// examples for how props is worked//
+
+// function App(props) {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//          <h1>Hello,{props.names} and {props.bills}</h1>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// examples for how props is worked//
+
+
+// class  App extends React.Component {
+//   render()
+//     {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//          <h1>Hello,{this.props.names} and {this.props.bills}</h1>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+//     };
+//   }
 
 function App() {
   return (
@@ -22,4 +99,6 @@ function App() {
   );
 }
 
-export default App;
+
+
+export default App; 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,346 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+// to show the realtime time in the browser//
+
+// function showcurrenttime() {
+//   const element =
+//     (
+//       <div>
+//         <h1>Current Time {new Date().toLocaleTimeString()}</h1>
+//         <h2>something the bigger</h2>
+//       </div>
+
+//     );
+//   const root = ReactDOM.createRoot(document.getElementById('root'));
+//   root.render(
+//     element,
+//   );
+
+// }
+
+// setInterval(showcurrenttime,1000)
+
+// to show the realtime time in the browser//
+
+// passing the props to the app.js//
+// const name="Robin";
+// const bill="Divya";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App names={name} bills={bill}/>
+//   </React.StrictMode>
+// );
+// passing the props to the app.js//
+
+// passing alert messages//
+// function Football() {
+//   const shoot = () => {
+//     alert("Great Shot!");
+//   }
+
+//   return (
+//     <button onClick={shoot}>Take the shot!</button>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+//   <Football />
+
+
+// );
+// passing alert messages//
+
+// passing arguments in alert messages//
+// function Football() {
+//   const shoot = (a) => {
+//     alert(a);
+//   }
+
+//   return (
+//     <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+//   <Football />
+
+
+// );
+// passing arguments in alert messages//
+
+// event in the alert messages//
+// function Football() {
+//   const shoot = (a, b) => {
+//     alert(b.type);
+//     /*
+//     'b' represents the React event that triggered the function.
+//     In this case, the 'click' event
+//     */
+//   }
+
+
+//   return (
+//     <button onClick={(event) => shoot("Goal!", event)}>Take the shot!</button>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+//   <Football />
+
+// );
+// event in the alert messages//
+
+// props can activate in the same page//
+// if else condition//
+
+// function MissedGoal() {
+// 	return <h1>MISSED!</h1>;
+// }
+
+// function MadeGoal() {
+// 	return <h1>GOAL!</h1>;
+// }
+
+// function Goal(props)
+// {
+//   const isGoal = props.isGoal;
+//   console.log(props.isGoal);
+//   if (isGoal > 1) {
+//     return <MadeGoal/>;
+//   }
+//   return <MissedGoal/>;
+// }
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+//  root.render(
+
+//   <Goal isGoal={1} />
+// );
+// props can activate in the same page//
+// if else condition//
+
+// The props passing another maethod//
+
+// function Carparking(props) {
+//   const cars = props.car;
+
+//   return (
+//     <>
+//       <h1>carparking</h1>
+
+
+//       <h2>You parked {cars.length} cars in your car parking</h2>
+
+
+//     </>
+//   );
+
+// }
+
+// const cars = ['benz', 'audi'];
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Carparking car={cars} />
+// );
+
+// The props passing another maethod//
+
+// list in react//
+function Car(props) {
+  return <li>I am a {props.brand}</li>;
+}
+
+function Garage() {
+  const cars = ['Ford', 'BMW', 'Audi', 'benz'];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car brand={car} />)}
+      </ul>
+    </>
+  );
+}
+
+function Fruits(props) {
+  return <li>i am a {props.carts}</li>;
+}
+
+function Cart() {
+  const carts = ['apple', 'mango', 'grapes'];
+  return (
+    <>
+      <h1>Here Are The Fruits</h1>
+      <ul>
+        {carts.map((veg) => <Fruits carts={veg} />)}
+      </ul>
+    </>
+  );
+}
+
+function Bikes(props) {
+  return <li>I am {props.brand} </li>
+}
+
+
+function Bikenames() {
+  const biker = [
+    {
+      id: '1',
+      brand: 'bajaj'
+    },
+    {
+      id: '2',
+      brand: 'yamaha'
+    }
+  ]
+  return (
+    <>
+      <h1>Here The Bikes Names</h1>
+      <ul>
+        {biker.map((twowheller) => <Bikes brand={twowheller.brand} />)}
+      </ul>
+    </>
+  )
+}
+
+function Myform() {
+  return (
+    <>
+      <h1>My Form</h1>
+      <form>
+        <label>Enter Your Name:
+          <input type="text" />
+        </label>
+      </form>
+    </>
+  )
+}
+
+// forms in react //
+
+function Form() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
+  return (
+    <>
+      <h1>
+        This Is My Form
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+          <input
+            type="text"
+            name="username"
+            value={inputs.username || ""}
+            onChange={handleChange}
+
+          />
+        </label>
+        <label>Enter your age:
+          <input
+            type="number"
+            name="age"
+            value={inputs.age || ""}
+            onChange={handleChange}
+
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
+
+
+function Form1() {
+  const [textarea, setTextarea] = useState(
+    "I Am Robin"
+  );
+
+  const handleChange = (event) => {
+    setTextarea(event.target.value)
+  }
+
+  return (
+    <>
+      <h1>This Is My TextArea</h1>
+      <form>
+        <textarea value={textarea} onChange={handleChange} />
+      </form>
+    </>
+  )
+}
+
+function Form2() {
+  const [vegetables, setvegetables] = useState("Carrot");
+
+  const handleChange = (event) => {
+    setvegetables(event.target.value)
+  }
+
+  return (
+    <>
+      <h1>Here Are The Options</h1>
+      <select value={vegetables} onChange={handleChange}>
+        <option value="carrot">Carrot</option>
+        <option value="tomato">Tomato</option>
+        <option value="ladiesfinger">Ladiesfinger</option>
+      </select>
+    </>
+
+  )
+}
+
+// forms in react //
+
+
+// redirecting to the another page //
+
+
+// redirecting to the another page //
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Garage />
+    <Cart />
+    <Bikenames />
+    <Myform />
+    <Form />
+    <Form1 />
+    <Form2 />
+  </div>
 );
+// list in react//
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
